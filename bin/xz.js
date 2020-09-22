16d9d7eefbb248a8d147b09042b9a81befdfc93b
xinze <xinzeedu@163.com>
2020-09-23 07:17:29 +0800

    feat: 新增 命令行界面

--- bin/xz.js
+++ bin/xz.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
 console.log('cli ... 666')
+const program = require('commander')
+program.version(require('../package.json').version)
+
+program
+    .command('init <name>')
+    .description('init project')
+    .action(name=>{
+        console.log('init' + name)
+    })
+    program.parse(process.argv)
