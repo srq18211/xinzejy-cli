82b3546465513fec7c9ae9f7d69d11616202d4c5
xinze <xinzeedu@163.com>
2020-09-23 07:36:57 +0800

    add download

--- bin/xz.js
+++ bin/xz.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
-console.log('cli ... 666')
 const program = require('commander')
 program.version(require('../package.json').version)
 
 program
     .command('init <name>')
     .description('init project')
-    .action(name=>{
-        console.log('init' + name)
-    })
+    .action(require('../lib/init'))
     program.parse(process.argv)
