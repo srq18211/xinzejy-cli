dbe803b224dbe5cb1db9dc56d798a68378743f69
xinze <xinzeedu@163.com>
2020-09-23 11:07:25 +0800

    新增页面

--- bin/xz.js
+++ bin/xz.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-const program = require('commander')
-program.version(require('../package.json').version)
+const program = require('commander');
+program.version(require('../package.json').version);
 
-program
-    .command('init <name>')
-    .description('init project')
-    .action(require('../lib/init'))
-    program.parse(process.argv)
+program.command('init <name>').description('init project').action(require('../lib/init'));
+
+program.command('g <name>').description('generate page,router').action(require('../lib/refresh'));
+program.parse(process.argv);
