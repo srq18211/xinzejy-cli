2293b47624e0474529a994ae2edc2d46c66ff274
shangriqiang <xinzeedu@163.com>
2020-10-12 07:13:30 +0800

    U 优化 generator 命令

--- bin/xz.js
+++ bin/xz.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
-const program = require('commander');
+const { Command } = require('commander');
+const program = new Command();
 program.version(require('../package.json').version);
 
-program.command('init <name>').description('init project').action(require('../lib/init'));
-
-program.command('g <name>').description('generate page,router').action(require('../lib/refresh'));
-program.parse(process.argv);
+// 初始化项目
+program
+    .command('init <name>')
+    .option('-t, --template <name>','初始化项目')
+    .description('初始化项目')
+    .action(require('../lib/init'));
+program
+    .command('g <name>')
+    .option('-t, --template <name>', '选择模板')
+    .description('生成页面，路由')
+    .action(require('../lib/refresh'));
+program
+    .parse(process.argv);
