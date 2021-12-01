4c42096e5309c5c9130b0b6d0e783d478f39bd53
xinze <xinzeedu@163.com>
2021-12-01 19:38:24 +0800

    添加node进程管理命令

--- bin/yy.js
+++ bin/yy.js
@@ -4,12 +4,16 @@ const program = new Command();
 program.version(require('../package.json').version);
 
 program.command('init <name>')
-  .description('init project')
+  .description('初始化项目')
   .action(require('../lib/init'));
 
 program.command('g <name>')
   // .option('-t --template', 'set template')
-  .description('generate page,router')
+  .description('生成页面，路由')
   .action(require('../lib/generat.js'));
+
+program.command('node [port]')
+  .description('查找指定端口的 node 进程')
+  .action(require('../lib/nodelist.js'))
 program.parse(process.argv);
 
